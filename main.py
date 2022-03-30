cb32eca1d42162b9afcd3b63c183c15b437c1438
Matthew J J <98533759+mattan27@users.noreply.github.com>
2022-03-30 01:53:48 +0000

    Ch. Lab 6

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
+import turtle
 
 def seq3np1(n):
-    """
-        Print the 3n+1 sequence from n, terminating when it reaches 1.
-        args: n (int) starting value for 3n+1 sequence
-        return: None
-    """
-    while(n != 1):
-        print(n)
-        if(n % 2) == 0:        # n is even
-            n = n // 2
-        else:                 # n is odd
-            n = n * 3 + 1
-    print(n)                  # the last print is 1
-
+  count = 0
+  while(n != 1):
+    if(n % 2) == 0:
+      n = n // 2
+    else:
+      n = n * 3 + 1
+    count = count + 1
+  return(count)
+  
+def drawGraph(max):
+  t = turtle.Turtle()
+  turtle.Screen()
+  t.shape("turtle")
+  t.goto(0,0)
+  t2 = turtle.Turtle()
+  t2.shape("turtle")
+  t2.color("red")
+  turtle.setworldcoordinates(0, 0, 10, 10)
+  max_so_far = 0
+  for i in range(1, max+1):
+    result = seq3np1(i)
+    if result > max_so_far:
+      max_so_far = result
+    t2.clear()
+    t2.goto(0,max_so_far)
+    label = "Maximum so far:", i, max_so_far
+    t2.write(label)
+    turtle.setworldcoordinates(0, 0, i+10, max_so_far+10)
+    t.pendown()
+    t.goto(i,max_so_far)
+    
 def main():
-	seq3np1(3)
+  x = input("Value:")
+  x = int(x)
+  if x > 0:
+    print("x > 0")
+  else:
+    quit()
+  for start in range(1, x+1):
+    n = seq3np1(start)
+    print(start, n)
+  drawGraph(x)
 main()
